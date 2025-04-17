881bd9cc23254dfd4f3c13ff7fcc01da7b755d3a
SakshamGarg77 <sgar0056@uni.sydney.edu.au>
2025-04-17 09:05:21 +0530

    Delete 2025 C LAB FINAL F directory

--- 2025 C LAB FINAL F/Inc/command.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMMAND_H
-#define COMMAND_H
-
-#include <stdint.h>
-
-//Callback function after the serial port receives information
-void on_receive_done(uint8_t *data, uint32_t len);
-
-#endif
